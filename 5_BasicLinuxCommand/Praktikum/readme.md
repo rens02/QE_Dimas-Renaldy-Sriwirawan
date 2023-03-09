5dc21c0dd9c89851717e8e8c16000690da0a7a89
rens02 <rens02@student.gunadarma.ac.id>
2023-03-10 02:11:46 +0700

    update

--- 5_BasicLinuxCommand/Praktikum/readme.md
+++ 5_BasicLinuxCommand/Praktikum/readme.md
@@ -1,3 +1,31 @@
+![path](assets/mkadirrmdir.jpg)
 ```
-AYO KITA COBA IN AWAWAWAW
+mkdir: Digunakan untuk membuat directory(folder) 
+rmdir : digunakan untuk menghapus directory 
+dir : digunakan untuk menampilkan isi pada directory aktif
 ```
+![path](assets/touch.jpg)
+```
+touch : digunakan untuk membuat file baru bernama dimas.txt
+rm: digunakan untuk menghapus file (bukan directory/folder)
+cd: digunakan untuk mengatur directory aktif(paramete .. berarti back ke sebelum directory aktif)
+```
+
+
+![path](assets/top.jpg)
+```
+touch digunakan untuk melihat seluruh proses yang berjalan dan melihat semua resource pada system
+```
+
+
+TUGAS 2 MEMBUAT PROGRAM SHELL
+![path](assets/tugas.jpg)
+```
+#!/bin/bash
+echo "Masukkan dua bilangan untuk dijumlahkan:"
+read bil1
+read bil2
+hasil=$(($bil1 + $bil2))
+echo "Hasil penjumlahan: $hasil"
+```
+read digunakan untuk meminta input
--- 6_BasicProgramming/Praktikum/basic_prio2_1.java
+++ 6_BasicProgramming/Praktikum/basic_prio2_1.java
@@ -5,19 +5,13 @@ public class basic_prio2_1 {
         int s = 0;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < height; j++) {
-                s=s+1;
-                if (s % 2 == 1) {
-                    if(s % 3 == 0){
-                        System.out.print("X ");
-                    }else{
-                        System.out.print("Y ");
-                    }
-                } else if (s % 2 == 0) {
-                    if(s % 3 == 0){
-                        System.out.print("X ");
-                    }else{
-                        System.out.print("Z ");
-                    }
+                s++;
+                if (s % 3 == 0) {
+                    System.out.print("X ");
+                } else if (s % 2 == 1) {
+                    System.out.print("Y ");
+                } else {
+                    System.out.print("Z ");
                 }
             }
             System.out.println();
